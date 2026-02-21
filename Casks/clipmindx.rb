02a0827d63c98d2366deb4a955a064530fdc8207
release-homebrew <release@localhost>
2026-02-21 15:08:51 +0000

    Update clipmindx to 0.0.4

--- Casks/clipmindx.rb
+++ Casks/clipmindx.rb
@@ -1,6 +1,6 @@
 cask "clipmindx" do
-  version "0.0.2"
-  sha256 "a50a40ed64e1982ab03c38a89e2632604f7686d2f475e2ff2535258e6212fcf2"
+  version "0.0.4"
+  sha256 "75a4f498b40fbe1fb616aecd94b350c395f9262c97bbddbef3a0412cccea2a33"
 
   url "https://github.com/admin-fractalx/clipmindx-mac/releases/download/v#{version}/ClipMindX-#{version}.zip"
   name "ClipMindX"
